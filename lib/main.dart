6589decfd2faf9054fc12951d6b25d82b2250ba1
/Aisurix <jinhawkins0606@gmail.com>
2024-09-29 12:19:47 +0800

    venz commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
- @override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: SignInPage(),
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignInPage(),
+    );
+  }
 }
